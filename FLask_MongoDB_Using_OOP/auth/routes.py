99d00dbf01ee226de4e597ae7758cc40b0ea68e6
Sikander Hussain <138971227+Sikandarh11@users.noreply.github.com>
2024-07-09 17:24:56 +0500

    Update routes.py

--- FLask_MongoDB_Using_OOP/auth/routes.py
+++ FLask_MongoDB_Using_OOP/auth/routes.py
@@ -17,7 +17,7 @@ class Register(Resource):
         phone_no = data.get('phone_no')
 
         if not username or not password or not name or not email or not phone_no:
-            return jsonify({"error": "Missing data"}), 400
+            return jsonify({"error": "missing data"}), 400
 
         hashed_pwd = hash_password(password)
 
@@ -42,21 +42,21 @@ class Login(Resource):
         password = data.get('password')
 
         if not username or not password:
-            return jsonify({"error": "Missing username or password"}), 400
+            return jsonify({"error": "missing username or password"}), 400
 
         try:
             document = request.app.db['users'].find_one({"username": username})
             if not document:
-                return jsonify({"error": "User not found"}), 404
+                return jsonify({"error": "user not found"}), 404
 
             if verify_password(password, document.get('password')):
                 user_id_str = str(document['_id'])
                 refresh_token, access_token = generate_tokens(user_id_str)
                 return jsonify({"refresh_token": refresh_token, "access_token": access_token}), 200
             else:
-                return jsonify({"error": "Invalid credentials"}), 401
+                return jsonify({"error": "invalid credentials"}), 401
         except Exception as e:
-            return jsonify({"error": f"Exception: {str(e)}"}), 500
+            return jsonify({"error": f"exception: {str(e)}"}), 500
 
 api.add_resource(Register, '/register')
 api.add_resource(Login, '/login')
